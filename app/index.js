3106517208a0115b62af8fcfb031db8faf986fc2
EQuimper <quimperemanuel@gmail.com>
2016-09-17 15:13:12 -0400

    Setup material

--- app/index.js
+++ app/index.js
@@ -3,16 +3,24 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import routes from './routes';
 import configureStore from './store/configureStore';
+import theme from './utils/theme.js';
 import './app.global.css';
+
+injectTapEventPlugin();
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+      <Router history={history} routes={routes} />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
